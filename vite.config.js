b0ae7659fdef0bfb8adccec8517a6f9c481310bc
Nguyhithatgud <nguyh432@gmail.com>
2025-07-15 18:42:05 +0700

    fix: blank page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'mjs']
+    }
+  }
 })
